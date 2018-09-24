b8c6f4bab6d7b06bdce27d9a85668fe22923dc2a
ryan <ryan.c@younify.com>
2018-09-24 12:18:15 +1000

    debug=false

--- common/urls.py
+++ common/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from common import settings
+from django.views import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('community.urls')),
+    url(r'^static/(?P<path>.*)$', static.serve,{'document_root':settings.STATIC_ROOT}),
 ]
